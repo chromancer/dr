780528da37d0d31ebdf430cc16e134f5cf5b4c9e
Kendon Barretta <chromancer@users.noreply.github.com>
2017-05-20 12:04:40 -0400

    upgrade to kick generic piles

--- mastertrain/collect.cmd
+++ mastertrain/collect.cmd
@@ -2,8 +2,8 @@ forage:
 put collect %1
 waitfor roundtime
 pausepart 2
-put kick %1
-pause 2
+put kick pile
+pause 1
 match leaving nearly locked
 match leaving mind lock
 match forage EXP HELP for more
@@ -11,9 +11,5 @@ put sk %2
 matchwait
 
 leaving:
-echo 
-echo * * *
 echo * * * Perception and outdoorsmanship locked. Exiting.
-echo * * *
-echo 
 Exit
